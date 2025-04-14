73b4b027f378dec75e681e589afc81b98edd2de8
JaspervdA <jasper1805@gmail.com>
2025-04-14 16:20:02 +0200

    Created 2024 folder and files and created a virtual environment and requirements file

---
+++ 2024/day1.py
@@ -0,0 +1,16 @@
+from aocd import get_data, submit
+
+year, day = 2024, 1
+data = get_data(year=year, day=day).splitlines()
+
+print(data)
+
+#answerA = 
+# submit(answerA, part="a", day=day, year=year)
+
+### Part B ###
+
+# Write your code here
+# answerB = 
+
+# submit(answerB, part="b", day=day, year=year)
